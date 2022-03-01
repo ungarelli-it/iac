52c7562077dbf05d0366bdf1ba54657d97c746aa
Roberto Ungarelli <roberto.ungarelli@zello.tec.br>
2022-03-01 16:11:33 -0300

    chore(sre): add iac

--- helms/providers.tf
+++ helms/providers.tf
@@ -4,6 +4,14 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
 
+provider "kubectl" {
+  load_config_file       = false
+  host                   = var.cluster_endpoint
+  token                  = var.cluster_token
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  apply_retry_count      = 5
+}
+
 provider "helm" {
   kubernetes {
     host                   = var.cluster_endpoint
